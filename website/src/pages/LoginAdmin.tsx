5823ac6a18debf192a3ef900878116d7b8d1ee74
Lahar_07 <148936173+Laharsolanki@users.noreply.github.com>
2026-02-03 18:58:23 +0530

    Center QR control heading and QR tiles

--- website/src/pages/LoginAdmin.tsx
+++ website/src/pages/LoginAdmin.tsx
@@ -56,6 +56,7 @@ export default function LoginAdmin() {
                 <FormField
                   control={form.control}
                   name="adminNumber"
+                  rules={{ required: "Admin number is required." }}
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Admin Number</FormLabel>
@@ -69,6 +70,10 @@ export default function LoginAdmin() {
                 <FormField
                   control={form.control}
                   name="password"
+                  rules={{
+                    required: "Password is required.",
+                    minLength: { value: 6, message: "Password must be at least 6 characters." }
+                  }}
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Password</FormLabel>
